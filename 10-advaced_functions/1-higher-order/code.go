69ef509edfe90a5a8cc595c2263eed48f5a55944
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-04-23 09:33:31 +0600

    first class and higher order function introduction in golang

--- 10-advaced_functions/1-higher-order/code.go
+++ 10-advaced_functions/1-higher-order/code.go
@@ -10,8 +10,12 @@ func mul (a, b int)	int {
 	return a * b
 }
 
-func aggregate(a, b)
+func aggregate(a, b, c int, arithmetic func(int, int) int) int {
+	return arithmetic(arithmetic(a, b), c)
+}
 
 func main () {
+	fmt.Println(aggregate(9, 2, 3, add)) // 6
+	fmt.Println(aggregate(9, 2, 3, mul)) // 54
 
 }
