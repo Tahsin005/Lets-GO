cb5a821581ce3f6a6b7e86948bb2a9bcb52d2297
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-04-24 08:11:02 +0600

    problem solving

---
+++ 0-problem-solving/13.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// https://codeforces.com/contest/2103/problem/A
+
+var (
+	in = bufio.NewReader(os.Stdin)
+	out = bufio.NewWriter(os.Stdout)
+)
+
+func main () {
+	var t, n, num int
+	fmt.Fscan(in, &t)
+
+	for range t {
+		fmt.Fscan(in, &n)
+		mp := map[int]bool{}
+
+		res := 0
+
+		for range n {
+			fmt.Fscan(in, &num)
+			_, ok := mp[num]
+			if !ok {
+				res++
+				mp[num] = true
+			}
+		}
+
+		fmt.Println(res)
+	}
+}
