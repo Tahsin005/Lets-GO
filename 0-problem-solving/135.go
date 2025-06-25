d7312eb6417e495a33ba3db0ae64fa11d9f41adc
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-06-25 06:18:23 +0600

    problem solving

---
+++ 0-problem-solving/135.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/description/?envType=problem-list-v2&envId=array
+
+func minimumOperations(nums []int) int {
+    count := 0
+    for i := 0; i < len(nums); i++ {
+        if (nums[i] - 1) % 3 == 0 || (nums[i] + 1) % 3 == 0 {
+            count++
+        }
+    }
+    return count
+}
+
+func main () {
+
+}
