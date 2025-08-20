e55618260b7d0a350433bb85da6a001ecacaf71b
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-08-20 22:28:15 +0600

    problem solving

--- 0-problem-solving/247.go
+++ 0-problem-solving/247.go
@@ -1,1 +1,33 @@
-https://leetcode.com/problems/count-square-submatrices-with-all-ones/description/?envType=daily-question&envId=2025-08-20
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/count-square-submatrices-with-all-ones/description/?envType=daily-question&envId=2025-08-20
+
+func countSquares(matrix [][]int) int {
+    m, n, sum := len(matrix), len(matrix[0]), 0
+    prev := make([]int, n)
+    for i := 0; i < m; i++ {
+        curr := make([]int, n)
+        for j := 0; j < n; j++ {
+            if matrix[i][j] == 1 {
+                curr[j] = 1 + min(get(prev, j - 1), get(prev, j), get(curr, j - 1))
+                sum += curr[j]
+            }
+        }
+        prev = curr
+    }
+
+    return sum
+}
+
+func get(row []int, idx int) int {
+    if idx < 0 {
+        return 0
+    }
+    return row[idx]
+}
+
+func main () {
+
+}
