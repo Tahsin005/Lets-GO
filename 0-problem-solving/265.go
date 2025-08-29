0935c691903ff504d05b59e25cab54cfb9dcfe81
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-08-29 09:35:30 +0600

    problem solving

---
+++ 0-problem-solving/265.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/alice-and-bob-playing-flower-game/description/?envType=daily-question&envId=2025-08-29
+
+func flowerGame(n int, m int) int64 {
+    return int64(n * m) / 2
+}
+
+func main () {
+
+}
