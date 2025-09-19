5fe2a816c2a5f643534ac73859bbc2307cd7b09c
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-09-19 15:27:18 +0600

    problem solving

---
+++ 0-problem-solving/307.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+func theMaximumAchievableX(num int, t int) int {
+    return num + 2 * t
+}
+
+func main () {
+
+}
