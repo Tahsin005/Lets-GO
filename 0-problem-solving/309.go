b8ad8b0df0bada2d4d2e4410550e18f33e92b66f
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-09-20 09:13:35 +0600

    problem solving

---
+++ 0-problem-solving/309.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/strictly-palindromic-number/description/
+
+func isStrictlyPalindromic(n int) bool {
+    return false
+}
+
+func main () {
+
+}
