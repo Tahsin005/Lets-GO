06a103b83f4bce645f61bc0449e03da7fedf41ce
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-10-09 21:34:45 +0600

    problem solving

--- 0-problem-solving/348.go
+++ 0-problem-solving/348.go
@@ -2,7 +2,32 @@ package main
 
 import "fmt"
 
-// 
+// https://leetcode.com/problems/design-parking-system/description/
+
+type ParkingSystem struct {
+    CarType map[int]int
+}
+
+
+func Constructor(big int, medium int, small int) ParkingSystem {
+    return ParkingSystem{
+        CarType: map[int]int{
+            1: big,
+            2: medium,
+            3: small,
+        },
+    }
+}
+
+
+func (this *ParkingSystem) AddCar(carType int) bool {
+    if this.CarType[carType] > 0{
+        this.CarType[carType] -= 1
+        return true
+    }
+
+    return false
+}
 
 func main () {
 
