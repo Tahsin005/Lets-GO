b04b57195a1c0291c78dff1f005a4f27e11619fe
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-10-11 20:52:02 +0600

    problem solving

---
+++ 0-problem-solving/352.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/integer-to-roman/description/
+
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func intToRoman(num int) string {
+    var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for num >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			num -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
+
+func main () {
+
+}
