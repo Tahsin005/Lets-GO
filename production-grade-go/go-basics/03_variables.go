ca62d9ad677a47025941273c08353f0af6d23671
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-02-03 23:24:19 +0600

    pggs

--- production-grade-go/go-basics/03_variables.go
+++ production-grade-go/go-basics/03_variables.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
 )
 
 var a, b bool
 
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
 func main() {
 	var i int
 	x := 4
-	fmt.Println(a, b, i, x)
+	var y = 10
+	var s string = "10"
+	fmt.Println(a, b, i, x, y, s)
+	fmt.Printf("%T %T %T %T %T %T\n", a, b, i, x, y, s)
+
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
