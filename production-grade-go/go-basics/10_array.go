0a740005b3fc5434d19ae81cdadcfeee12692a95
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-02-08 00:32:27 +0600

    pggs

--- production-grade-go/go-basics/10_array.go
+++ production-grade-go/go-basics/10_array.go
@@ -7,10 +7,4 @@ func main() {
 	a[0] = "hello"
 	a[1] = "world"
 	fmt.Println(a, a[0], a[1])
-
-	arr := [2]string{"hello", "world"}
-	fmt.Println(arr)
-
-	arr2 := [2]int{1, 2}
-	fmt.Println(arr2)
 }
