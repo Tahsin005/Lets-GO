df3dd25fa261555e1a199dd3da0461ba81814069
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-02-08 01:00:24 +0600

    pggs

---
+++ production-grade-go/go-basics/11_slices.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func main() {
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+	var s []int = primes[1:4]
+	fmt.Println(s)
+
+	fmt.Println(len(s))
+	fmt.Println(cap(s)) // capacity is the number of elements in the underlying array (starting from the first element in ths slice to the last element in the array)
+
+	fmt.Println("----------------")
+
+	x := []int{2, 3, 5, 7, 11, 13}
+
+	x = x[1:4]
+	fmt.Println(x)
+
+	x = x[:2]
+	fmt.Println(x)
+
+	x = x[1:]
+	fmt.Println(x)
+
+	fmt.Println("----------------")
+
+	y := []int{2, 3, 5, 7, 11, 13}
+	printSlice(y)
+
+	y = y[:0]
+	printSlice(y)
+
+	y = y[:4]
+	printSlice(y)
+
+	y = y[2:]
+	printSlice(y)
+
+	fmt.Println("----------------")
+
+	var slice []int
+	fmt.Println(slice, len(slice), cap(slice))
+	if slice == nil {
+		fmt.Println("zero value of a slice is nil")
+	}
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
