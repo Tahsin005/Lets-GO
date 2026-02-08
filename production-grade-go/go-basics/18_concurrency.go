962e47c966fa9489cf0748d30f48d9df33f5c9c4
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-02-08 17:17:48 +0600

    pggs

--- production-grade-go/go-basics/18_concurrency.go
+++ production-grade-go/go-basics/18_concurrency.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	"fmt"
-	"time"
+	// "time"
 )
 
-func say(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
+// func say(s string) {
+// 	for i := 0; i < 5; i++ {
+// 		time.Sleep(100 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func sum(s []int, c chan int) {
+	total := 0
+	for _, v := range s {
+		total += v
 	}
+	c <- total
 }
 
 func main() {
-	go say("world")
-	say("hello")
+	// go say("world")
+	// say("hello")
+
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+
+	x := <-c // receiving from channel is a blocking execution
+	y := <-c
+
+	fmt.Println(x, y, x+y)
 }
