9a202e6ba2b133cb25ddd50568d4e1fddcdc2cfc
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-02-08 15:03:04 +0600

    pggs

--- production-grade-go/go-basics/16_interface.go
+++ production-grade-go/go-basics/16_interface.go
@@ -9,6 +9,29 @@ type Abser interface {
 	Abs() float64
 }
 
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Println("int", v + 2)
+	case string:
+		fmt.Println("string", v + " world")
+	default:
+		fmt.Println("unknown")
+	}
+}
+
 func main() {
 	var a Abser
 	f := MyFloat(-math.Sqrt2)
@@ -20,6 +43,15 @@ func main() {
 	// a = v // error: cannot use v (variable of type Verteyy) as Abser value in assignment: Verteyy does not implement Abser (Abs method has pointer receiver)
 
 	fmt.Println(a.Abs())
+
+	var i I = T{"hello"}
+
+	// only after concrete type is known, we can call the method
+	i.M()
+
+	do(21)
+	do("hello")
+	do(21.21)
 }
 
 type MyFloat float64
