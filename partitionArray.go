b0d384010992c7b3b9398d5effbaa11f0200f12c
MD. Tahsin Ferdous <124136023+Tahsin005@users.noreply.github.com>
2026-04-09 12:29:16 +0600

    Create partitionArray.go

---
+++ partitionArray.go
@@ -0,0 +1,14 @@
+func partitionArray(nums []int, k int) int {
+    sort.Ints(nums)
+    ans := 1
+    l := 0
+
+    for i := 1; i < len(nums); i++ {
+        if nums[i] - nums[l] > k {
+            ans++
+            l = i
+        }
+    }
+
+    return ans
+}
