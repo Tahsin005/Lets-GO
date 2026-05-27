084cbccf2d2bee2e88a9407c753e9f2cf70ec11d
MD. Tahsin Ferdous <124136023+Tahsin005@users.noreply.github.com>
2026-05-27 10:46:07 +0600

    Create numberOfSpecialChars_.go

---
+++ numberOfSpecialChars_.go
@@ -0,0 +1,20 @@
+func numberOfSpecialChars(word string) int {
+    small, capital := make(map[rune]int, 26), make(map[rune]int, 26)
+    count := 0
+    for i, char := range word {
+        if char >= 'a' && char <= 'z' {
+            small[char] = i
+        } else if char >= 'A' && char <= 'Z' {
+            if _, exists := capital[char]; !exists {
+                capital[char] = i
+            }
+        }
+    }
+    for key, value := range small {
+        if value < capital[key - 32] {
+            count++
+        }
+    }
+    
+    return count
+}
